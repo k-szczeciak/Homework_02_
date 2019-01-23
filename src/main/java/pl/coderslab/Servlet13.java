75f08258111530a7222b6d11c152572269a64d85
k-szczeciak <szczeciak@hotmail.com>
2019-01-23 06:17:25 +0100

    Zadanie 4

--- src/main/java/pl/coderslab/Servlet13.java
+++ src/main/java/pl/coderslab/Servlet13.java
@@ -13,15 +13,20 @@ import java.util.Collections;
 
 @WebServlet("/Servlet13")
 public class Servlet13 extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-    }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //setting up charset for displaing the pache with polish characters:
         response.setContentType("text/html; charset=utf-8");
+
+        //getting path for the file:
+        // /Users/krzysztofszczeciak/workspace/Homework_02/src/main/java/pl/coderslab/oop.txt <-- path
+        // pl/coderslab/oop.txt <-- relative path
+        // /Users/krzysztofszczeciak/workspace/Homework_02/target/Homework_02-1.0-SNAPSHOT/ <-- path retrieved form:
         String path1 = getServletContext().getRealPath("/");
         response.getWriter().append("application path: ").append(path1).append("<br>").append("<br>");
-        Path path = Paths.get(path1 + "/oop.txt");
+        Path path = Paths.get("/Users/krzysztofszczeciak/workspace/Homework_02/src/main/java/pl/coderslab/oop.txt");
+
 
         try{
         for (String line : Files.readAllLines(path)) {
---
+++ src/main/java/pl/coderslab/Servlet141.java
@@ -0,0 +1,27 @@
+package pl.coderslab;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/Servlet141")
+public class Servlet141 extends HttpServlet {
+
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html; charset=utf-8");
+        for (int i = 0; i < 8; i++) {
+            response.getWriter().append("<a href=\"/Servlet142")
+                    .append("?id=")
+                    .append(String.valueOf(i))
+                    .append("\">LInk do Id: ")
+                    .append(String.valueOf(i))
+                    .append("</a>")
+            .append("<br>");
+        }
+
+    }
+}
